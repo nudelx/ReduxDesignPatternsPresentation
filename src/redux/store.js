09ccadbddb139945470b6e57abe9bfd50e585c1d
nudelx <alex.nudelman@samanage.com>
2018-06-21 15:45:00 +0300

    init

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,11 @@
 import { createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import cases from './reducers/casesReducer';
-import ui from './reducers/uiReducer';
+import cases from './reducers/casesReducer'
+import ui from './reducers/uiReducer'
+import notify from './reducers/notifyReducer'
 
-const rootReducer = combineReducers({ cases, ui })
+const rootReducer = combineReducers({ cases, ui, notify })
 
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-
-
-
-
 
 const enhancer = composeEnhancers(
   applyMiddleware(...[]),
