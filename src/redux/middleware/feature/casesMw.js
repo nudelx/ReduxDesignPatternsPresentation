608557dade1a732f146dbd7f4f58f8f30e869be7
nudelx <alex.nudelman@samanage.com>
2018-07-01 16:20:27 +0300

    json

--- src/redux/middleware/feature/casesMw.js
+++ src/redux/middleware/feature/casesMw.js
@@ -5,7 +5,7 @@ import { setNotify } from '../../actions/notifyAction';
 import { fetchFields } from '../../actions/fieldsActions'
 import { conditionalFetch } from '../../actions/conditionAction'
 
-const url = 'case.jsons'
+const url = 'case.json'
 const normalizeKey = 'Id'
 const fieldsCondition = state => !Object.keys(state.fields).length
 
